8305a637a8836a0a5dd9ad37f87cdd92291c8322
Rebecca Risha <rebecca.risha@rubicotech.in>
2022-12-05 10:03:48 +0530

    added db mysql and set up express js

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+// get the client
+const mysql = require("mysql2");
+const app = express();
+const port = 3000;
+
+// create the connection to database
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "rebeccarisha",
+  password: "Rubi@123",
+  database: "west_river_academy",
+});
+
+// simple query
+connection.query("SELECT * FROM `users`", function (err, results, fields) {
+  console.log(results); // results contains rows returned by server
+  //   console.log(fields); // fields contains extra meta data about results, if available
+});
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/posts", (req, res) => {
+  res.send([
+    {
+      id: 1,
+      title: "first post",
+    },
+  ]);
+});
+              
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
